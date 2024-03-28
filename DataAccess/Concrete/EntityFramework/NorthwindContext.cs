8791f627798bb7ba1c3daabb2056ab7a9fb5027b
kubrastack <kmrt3014@gmail.com>
2024-03-28 11:47:02 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        //Bu method hangi veri tabanı ile ilişkilendirdiğimizi açıklar
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=KUBRA;Initial Catalog=Northwind;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
